990b2a6cd53efc478cf8a675601f70f8947b9251
Daniel Gray <d.gray@dickblick.com>
2017-07-30 10:55:32 -0700

    [master] Adding some basic starter things.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Jeroboam',
+    version='0.1dev',
+    packages=['jeroboam',],
+    license='MIT',
+    long_description=open('README.rst','r').read()
+)
