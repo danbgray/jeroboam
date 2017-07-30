32dfa6217b3cfe3c20bc2a6ddbd5747a965aa5cf
Daniel Gray <d.gray@dickblick.com>
2017-07-30 11:53:02 -0700

    [master] Adding basic requirements.

---
+++ jeroboam/__init__.py
@@ -0,0 +1,4 @@
+__author__ = "Daniel Gray"
+__author_email__ = "eigensix@gmail.com"
+__version__ = "0.1dev"
+__license__ = 'MIT'
--- setup.py
+++ setup.py
@@ -1,9 +1,26 @@
+import sys
 from distutils.core import setup
 
-setup(
-    name='Jeroboam',
-    version='0.1dev',
-    packages=['jeroboam',],
-    license='MIT',
-    long_description=open('README.rst','r').read()
-)
+if sys.version_info < (3, 2):
+    raise NotImplementedError(
+        "Sorry, you need at least Python 3.2+ or Python 3.2+ to use jeroboam.")
+
+import jeroboam  # NOQA
+
+setup(name='Jeroboam',
+      version=jeroboam.__version__,
+      author=jeroboam.__author__,
+      author_email=jeroboam.__author_email__,
+      license=jeroboam.__license__,
+      packages=['jeroboam'],
+      long_description=open('README.rst', 'r').read(),
+      install_requires=[
+          'click>=4.0',
+          'Werkzeug>=0.9'
+      ],
+      entry_points={
+          'console_scripts': [
+              'jb = jeroboam.cli.management:main'
+          ]
+      }
+      )
